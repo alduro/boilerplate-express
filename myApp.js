19e2068554c98a2124fb11bc0cfaa51322d0e43c
Aldo Nievas <aldonievas@MacBook-Pro-de-Aldo.local>
2024-01-17 19:18:01 -0300

    middleware time

--- myApp.js
+++ myApp.js
@@ -21,6 +21,13 @@ app.get('/json', function(req, res) {
   res.json({"message": "Hello json"})
 });
 
+app.get('/now', function(req, res, next){
+  req.time = new Date().toString()
+  next()
+}, function(req, res){
+  res.json({"time": req.time})
+});
+
 
 
 
